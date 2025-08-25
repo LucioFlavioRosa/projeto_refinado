5a732bf3066e87bd8d53ec51550bbb0679d31089
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-08-24 23:59:32 -0300

    feat: backend/app/core/security.py

---
+++ backend/app/core/security.py
@@ -0,0 +1,19 @@
+from passlib.context import CryptContext
+from jose import jwt
+from app.core.config import settings
+from datetime import datetime, timedelta
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(hours=2)):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+    return encoded_jwt
